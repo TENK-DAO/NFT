9ebe7b7cf558a149cf211e08dce321dc51c17e12
Willem Wyndham <wyndham@cs.unc.edu>
2022-03-22 13:26:34 -0400

    feat: upgrade to newest sdk

--- test-token-receiver/src/lib.rs
+++ test-token-receiver/src/lib.rs
@@ -4,17 +4,14 @@ A stub contract that implements nft_on_transfer for simulation testing nft_trans
 use near_contract_standards::non_fungible_token::core::NonFungibleTokenReceiver;
 use near_contract_standards::non_fungible_token::TokenId;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::json_types::ValidAccountId;
 use near_sdk::{
-    env, ext_contract, log, near_bindgen, setup_alloc, AccountId, Balance, Gas, PanicOnDefault,
+    env, ext_contract, log, near_bindgen, require, AccountId, Balance, Gas, PanicOnDefault,
     PromiseOrValue,
 };
 
-setup_alloc!();
-
-const BASE_GAS: Gas = 5_000_000_000_000;
-const PROMISE_CALL: Gas = 5_000_000_000_000;
-const GAS_FOR_NFT_ON_TRANSFER: Gas = BASE_GAS + PROMISE_CALL;
+const BASE_GAS: u64 = 5_000_000_000_000;
+const PROMISE_CALL: u64 = 5_000_000_000_000;
+const GAS_FOR_NFT_ON_TRANSFER: Gas = Gas(BASE_GAS + PROMISE_CALL);
 
 const NO_DEPOSIT: Balance = 0;
 
@@ -38,8 +35,8 @@ trait ValueReturnTrait {
 #[near_bindgen]
 impl TokenReceiver {
     #[init]
-    pub fn new(non_fungible_token_account_id: ValidAccountId) -> Self {
-        Self { non_fungible_token_account_id: non_fungible_token_account_id.into() }
+    pub fn new(non_fungible_token_account_id: AccountId) -> Self {
+        Self { non_fungible_token_account_id }
     }
 }
 
@@ -60,9 +57,8 @@ impl NonFungibleTokenReceiver for TokenReceiver {
         msg: String,
     ) -> PromiseOrValue<bool> {
         // Verifying that we were called by non-fungible token contract that we expect.
-        assert_eq!(
-            &env::predecessor_account_id(),
-            &self.non_fungible_token_account_id,
+        require!(
+            env::predecessor_account_id() == self.non_fungible_token_account_id,
             "Only supports the one non-fungible token contract"
         );
         log!(
@@ -77,13 +73,8 @@ impl NonFungibleTokenReceiver for TokenReceiver {
             "return-it-later" => {
                 let prepaid_gas = env::prepaid_gas();
                 let account_id = env::current_account_id();
-                ext_self::ok_go(
-                    true,
-                    &account_id,
-                    NO_DEPOSIT,
-                    prepaid_gas - GAS_FOR_NFT_ON_TRANSFER,
-                )
-                .into()
+                ext_self::ok_go(true, account_id, NO_DEPOSIT, prepaid_gas - GAS_FOR_NFT_ON_TRANSFER)
+                    .into()
             }
             "keep-it-now" => PromiseOrValue::Value(false),
             "keep-it-later" => {
@@ -91,13 +82,13 @@ impl NonFungibleTokenReceiver for TokenReceiver {
                 let account_id = env::current_account_id();
                 ext_self::ok_go(
                     false,
-                    &account_id,
+                    account_id,
                     NO_DEPOSIT,
                     prepaid_gas - GAS_FOR_NFT_ON_TRANSFER,
                 )
                 .into()
             }
-            _ => env::panic(b"unsupported msg"),
+            _ => env::panic_str("unsupported msg"),
         }
     }
 }
